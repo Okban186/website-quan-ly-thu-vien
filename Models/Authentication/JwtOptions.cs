68329039df9f258d6ab8aa93c7711eaf9cd8eeb9
Okban186 <ungthu374@gmail.com>
2026-09-22 01:12:48 +0700

    feat: thêm cấu hình tùy chọn cho JWT
    - Định nghĩa JwtOptions để quản lý các thiết lập JWT.
    - Cấu hình Issuer, Audience và Key cho việc xác thực token.
    - Thiết lập thời gian sống của Access Token và Refresh Token.

---
+++ Models/Authentication/JwtOptions.cs
@@ -0,0 +1,15 @@
+namespace WebsiteQuanLyThuVien.Models.Authentication;
+
+public class JwtOptions
+{
+
+    public string Issuer { get; set; } = string.Empty;
+
+    public string Audience { get; set; } = string.Empty;
+
+    public string Key { get; set; } = string.Empty;
+
+    public int AccessTokenMinutes { get; set; }
+
+    public int RefreshTokenDays { get; set; }
+}
